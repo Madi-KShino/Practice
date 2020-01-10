a80bc02ae4676e8b96ae934f2636b19815e045f6
Madi <madison.k.shino@gmail.com>
2020-01-09 23:34:23 -0700

    First

---
+++ PracticeGit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PracticeGit
+//
+//  Created by Madison Kaori Shino on 1/9/20.
+//  Copyright © 2020 Madi S. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    //Outlets
+    @IBOutlet weak var HelloWorldLabel: UILabel!
+    @IBOutlet weak var secondLabel: UILabel!
+    
+    //Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
